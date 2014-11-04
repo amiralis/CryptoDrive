7a845143bcba2ff6c9cc8c1a0073376c2d1ff60b
Amirali Sanatinia <amiralis@users.noreply.github.com>
2014-11-04 08:15:42 -0500

    Add fusepy

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 __author__ = 'amirali'
 
+from distutils.core import setup
+
+setup(name='cryptobox',
+      version='1.0',
+      description='Crypto Box: a Virtual Encrypted Filesystem',
+      author='Amirali Sanatinia',
+      author_email='amirali@ccs.neu.edu',
+      url='https://github.com/amiralis/CryptoBox',
+      packages=['cryptography', 'fusepy'],
+     )
