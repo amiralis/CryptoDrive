501fe9c0fdfee0613d49064aff3a09ce2eb69cb8
Amirali Sanatinia <amiralis@users.noreply.github.com>
2014-11-05 21:12:14 -0500

    Fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-__author__ = 'amirali'
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-from distutils.core import setup
+import cryptodrive
 
-setup(name='cryptobox',
-      version='1.0',
-      description='Crypto Box: a Virtual Encrypted Filesystem',
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='cryptodrive',
+      version=cryptodrive.__version__,
+      description='CryptoDrive: a Virtual Encrypted Filesystem',
+      long_description=readme(),
       author='Amirali Sanatinia',
       author_email='amirali@ccs.neu.edu',
-      url='https://github.com/amiralis/CryptoBox',
-      packages=['cryptography', 'fusepy'],
-     )
+      url='https://github.com/amiralis/CryptoDrive',
+      packages=['cryptodrive'],
+      scripts=['bin/crypto-drive'],
+      license='Apache 2',
+      classifiers=['License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.7'],
+      install_requires=['fusepy', 'cryptography'],
+      include_package_data=True)
